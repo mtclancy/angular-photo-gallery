e0c26de0f54838c0d53f682a42ad61cc20c81033
Michael Clancy <mike@wewritecode.com>
2023-12-03 21:40:53 -0600

    moved proxy server to environments

--- browser/src/app/environments/environment.ts
+++ browser/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:81/api',
-  albumApiUrl: 'https://jsonplaceholder.typicode.com/photos'
+  albumApiUrl: 'https://jsonplaceholder.typicode.com/photos',
+  proxyServerUrl: 'http://localhost:3000',
 };
--- browser/src/app/shared/directives/download-image.directive.ts
+++ browser/src/app/shared/directives/download-image.directive.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Directive, HostListener, Input } from '@angular/core';
+import { environment } from 'src/app/environments/environment';
 
 @Directive({
   selector: '[downloadImage]',
@@ -18,9 +19,9 @@ export class DownloadImageDirective {
     if (!this.fileUrl) {
       return;
     }
-    const fileUrl =
-      'http://localhost:3000/proxy-image?url=' +
-      encodeURIComponent(this.fileUrl);
+    const fileUrl = `${
+      environment.proxyServerUrl
+    }/proxy-image?url=${encodeURIComponent(this.fileUrl)}`;
     this.http.get(fileUrl, { responseType: 'blob' }).subscribe((blob) => {
       const imageBlob = new Blob([blob]);
       const link = document.createElement('a');
