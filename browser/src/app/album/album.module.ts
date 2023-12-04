fbcd65bf087d6362a31334fd7bffe49924f90a0d
Michael Clancy <mike@wewritecode.com>
2023-12-04 08:25:19 -0600

    cdk virtual scroll added, grid view issue

--- browser/src/app/album/album.module.ts
+++ browser/src/app/album/album.module.ts
@@ -6,9 +6,16 @@ import { CommonModule } from '@angular/common';
 import { AlbumService } from './services/album.service';
 import { LayoutsModule } from '../layouts/layouts.module';
 import { SharedModule } from '../shared/shared.module';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  imports: [CommonModule, AlbumRoutingModule, LayoutsModule, SharedModule],
+  imports: [
+    CommonModule,
+    AlbumRoutingModule,
+    LayoutsModule,
+    SharedModule,
+    ScrollingModule,
+  ],
   exports: [],
   declarations: [AlbumContainerComponent, AlbumsComponent],
   providers: [AlbumService],
